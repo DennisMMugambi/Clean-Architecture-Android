19e4d77d8768127c42c23b009fb5f6237d70cbb2
mugambi.dennis <mugambi.dennis@eclectics.io>
2023-02-20 15:56:15 +0300

    Complete application

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/MainActivity.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/MainActivity.kt
@@ -8,15 +8,38 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.plcoding.cryptocurrencyappyt.Presentation.Screen
+import com.plcoding.cryptocurrencyappyt.Presentation.coin_detail.CoinDetailScreen
+import com.plcoding.cryptocurrencyappyt.Presentation.coin_list.CoinListScreen
 import com.plcoding.cryptocurrencyappyt.Presentation.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptocurrencyAppYTTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                           composable(
+                               route = Screen.CoinListScreen.route
+                           ) {
+                               CoinListScreen(navController)
+                           }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
